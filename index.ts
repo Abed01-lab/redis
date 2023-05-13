690288c28bb61c3fedf18cf7030ab932a4a44f31
ABHA1 <abha1@ferring.com>
2023-05-13 16:22:31 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import { createClient } from 'redis';
+
+
+const app = express();
+const client = createClient();
+
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+	console.log(req.body)
+	const { key_field, value_field } = req.body
+	console.log(key_field, value_field)
+  await client.connect();
+  await client.set(key_field, value_field)
+  await client.expire(key_field, 10)
+  await client.disconnect();
+  res.status(203);
+});
+
+app.get('/', async (req, res) => {
+  await client.connect();
+  const data = await client.keys("*")
+  await client.disconnect();
+  res.json(data);
+});
+
+app.get('/:key', async (req, res, ) => {
+	const key_field = req.params.key;
+	await client.connect();
+	const data = await client.get(key_field)
+	await client.disconnect();
+	res.json(data);
+});
+
+app.delete('/:key', async (req, res, ) => {
+	const key_field = req.params.key;
+	await client.connect();
+	const data = await client.del(key_field)
+	await client.disconnect();
+	res.json(data);
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
